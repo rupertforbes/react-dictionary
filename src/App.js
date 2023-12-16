ea05287b98da43c6eb0385704c20bd6cbb0eb836
rupertforbes <127436772+rupertforbes@users.noreply.github.com>
2023-12-16 19:59:58 +0000

    imported dictionary logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="logo" alt="dictionary logo" />
         <p>Hello from React</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
